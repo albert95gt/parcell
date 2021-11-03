ab5152a52f27cb53316264b72afa7398f9fb7c89
Albert Takács <albitakacs@gmail.com>
2021-11-03 18:58:13 +0100

    add

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
 
+// const date1 = Date.now();
+// // console.log(date1);
+
+// setTimeout(function() {
+//     const date2 = Date.now();
+
+//     console.log(date1);
+//     console.log(date2);
+//     console.log(date2 - date1);
+// }, 3000);
+// =================== //
+
+const timer = {
+    start(){
+        const startTime = Date.now()
+        setInterval(() => {
+           const currentTime = Date.now();
+
+        //    console.log(currentTime - startTime);
+        }, 1000);
+    }
+}
+
+timer.start()
